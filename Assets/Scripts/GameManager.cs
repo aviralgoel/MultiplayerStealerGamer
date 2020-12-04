f314dd0e1af6f14ab8beef0b7e53e43f7d9f333b
aviralgoel <aviralgoel@rocketmail.com>
2020-12-04 17:41:38 +0400

    added playerController and Game Manager
    almost done with Game Manager and completely done with player controller.
    
    yet to sync everything on the network

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    [Header("Game Stats")]
+    public bool hasGameEnded = false;
+    public float timeToWin;
+    public float invincibilityDuration;
+    public float hatPickupTime;
+
+    [Header("Player")]
+    public string playerPrefabLocation;
+    public Transform[] spawnPoints;
+    public PlayerController[] players;
+    public int playerWithHatID;
+    private int playersInGame;
+
+
+    public static GameManager instance;
+    //instance
+    private void Awake()
+    {
+        // Make sure there is only one instance of this GameManager existing
+        if (instance != null & instance != this)
+            gameObject.SetActive(false);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        //Set the length of PlayerController array to the number of players determined by PhotonNetwork
+        players = new PlayerController[PhotonNetwork.PlayerList.Length];
+        photonView.RPC("ImInGame", RpcTarget.AllBuffered);
+
+    }
+
+
+    // Each player tells the GameManager that they've joined the game and check if we are ready to start the game
+    // it is PUNRPC because we need to notify everyother player on the network about number of players who are ready
+    [PunRPC]
+    private void ImInGame()
+    {
+        playersInGame++;
+
+        // if everyone joined the game scene, then spawn all.
+        if(playersInGame == PhotonNetwork.PlayerList.Length)
+        {
+            SpawnThePlayer()
+        }
+    }
+
+    private void SpawnThePlayer()
+    {
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabLocation, 
+                                   spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+
+        PlayerController playerScript = playerObj.GetComponent<PlayerController>();
+
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+
+public class PlayerController : MonoBehaviour
+{
+    [HideInInspector]
+    //player ID to identify the player in the multiplayer game
+    public int id;
+
+
+    [Header("Player Properties")]
+    public float moveSpeed;
+    public float jumpForce;
+    public GameObject hatObject;
+    
+    [HideInInspector]
+    public float currentHatTime;
+
+    [Header("Components")]
+    public Rigidbody rb;
+    public Player photonPlayer;
+
+    //ray shot by player towards the groud, to check isGrounded?
+    private float rayLength = 0.7f;
+
+
+
+    private void Update()
+    {
+        Move();
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            TryJump();
+        }
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxis("Horizontal") * moveSpeed;
+        float v = Input.GetAxis("Vertical") * moveSpeed;
+
+        rb.velocity = new Vector3(h, rb.velocity.y, v);
+
+
+    }
+
+    void TryJump()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        
+        //if player on ground
+        if(Physics.Raycast(ray, rayLength))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
